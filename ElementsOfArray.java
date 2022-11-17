eb2d1bf720c279a6b305474176b41acd7eea9444
Sreedhar1918 <nagalli.sreedhar.20.1@mountblue.tech>
2022-11-17 08:18:53 +0530

    added ElementsOfArray.java file

---
+++ ElementsOfArray.java
@@ -0,0 +1,13 @@
+//1) Java Program to print the elements of an array
+
+public class ElementsOfArray{
+    public static void main(String[] args){
+        //Intialize an array
+        int[] arr = new int[] {1,2,3,4,5};
+        System.out.print("Elements of given array :");
+         //Loop through the array by incrementing value of i  
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
